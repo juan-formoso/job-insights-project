5009410e44dbd71290aa31fb4bfe7dddbea281e5
Avery <trajano.juan@gmail.com>
2022-06-05 20:41:51 -0300

    style: :art: Atualiza test_counter.py em /tests/counter
    Troca aspas duplas por simples e ordem de execução dos testes.
    
    Corrige erros de lint.

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,6 +2,6 @@ from src.counter import count_ocurrences
 
 
 def test_counter():
-    assert count_ocurrences("src/jobs.csv", "javascript") = 122
-    assert count_ocurrences("src/jobs.csv", "python") = 1639
+    assert count_ocurrences('src/jobs.csv', 'python') == 1639
+    assert count_ocurrences('src/jobs.csv', 'javascript') == 122
     

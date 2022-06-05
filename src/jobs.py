3ecc4bce35f961743920380613442b70f3b9c3d3
Avery <trajano.juan@gmail.com>
2022-06-05 15:57:42 -0300

    feat: :sparkles: Atualiza jobs.py in /src
    Implementa função read
    
    Finaliza o requisito 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,19 +1,22 @@
 from functools import lru_cache
-# comment to commit
+import csv
 
 
 @lru_cache
 def read(path):
     """Reads a file from a given path and returns its contents
-
     Parameters
     ----------
     path : str
         Full path to file
-
     Returns
     -------
     list
         List of rows as dicts
     """
-    return []
+    jobs = []
+    with open(path, encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            jobs.append(row)
+    return jobs

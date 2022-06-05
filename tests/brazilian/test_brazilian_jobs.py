5038f20f8b37a3a1531cf1c7750740854126b9f5
Avery <trajano.juan@gmail.com>
2022-06-05 20:50:16 -0300

    chore: :art: Atualiza test_counter.py em /tests/counter; Atualiza test_brazilian_jobs.py em /tests/brazilian
    Corrige erros de lint

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -6,4 +6,4 @@ def test_brazilian_jobs():
     Test the Brazilian Jobs file.
     """
     jobs = read_brazilian_file('tests/mocks/brazilian_jobs.csv')
-    assert ('title' and 'salary' and 'type') in list (jobs[0].keys())
+    assert ('title' and 'salary' and 'type') in list(jobs[0].keys())
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -4,4 +4,3 @@ from src.counter import count_ocurrences
 def test_counter():
     assert count_ocurrences('src/jobs.csv', 'python') == 1639
     assert count_ocurrences('src/jobs.csv', 'javascript') == 122
-

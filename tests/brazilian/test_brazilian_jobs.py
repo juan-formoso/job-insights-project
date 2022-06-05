8cdbe9d89b7a8ae003b994a78dad3e7fc9faf5ea
Avery <trajano.juan@gmail.com>
2022-06-05 20:45:18 -0300

    test: :sparkles: Atualiza test_brazilian_jobs.py em /tests/brazilian
    Implementa testes para o arquivo brazilian_jobs.csv
    
    Finaliza o requisito 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    """
+    Test the Brazilian Jobs file.
+    """
+    jobs = read_brazilian_file('tests/mocks/brazilian_jobs.csv')
+    assert ('title' and 'salary' and 'type') in list (jobs[0].keys())

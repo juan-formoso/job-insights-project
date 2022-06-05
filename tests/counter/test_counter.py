d3921ccdfc4eaeaa5e95f5e387be3c5ea91172e2
Avery <trajano.juan@gmail.com>
2022-06-05 20:46:16 -0300

    style: :art: Atualiza test_counter.py em /tests/counter
    Apaga espaço em branco na linha 7
    
    Corrige erro de lint

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -4,4 +4,4 @@ from src.counter import count_ocurrences
 def test_counter():
     assert count_ocurrences('src/jobs.csv', 'python') == 1639
     assert count_ocurrences('src/jobs.csv', 'javascript') == 122
-    
+

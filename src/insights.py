dd1fc5aaefccdfdc52f6cb12b133600dcc6a8ac4
Avery <trajano.juan@gmail.com>
2022-06-05 19:01:22 -0300

    feat: :sparkles: Atualiza insights.py em /src
    Implementa função get_unique_industries
    
    Finaliza requisito 3

--- src/insights.py
+++ src/insights.py
@@ -56,7 +56,12 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+    jobs = read(path)
+    industries = []
+    for job in jobs:
+        if job["industry"] not in industries and job["industry"] != "":
+            industries.append(job["industry"])
+    return industries
 
 
 def filter_by_industry(jobs, industry):

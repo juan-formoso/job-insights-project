e611301dc4e52f510fd7f125e1fdca9838a8df0f
Avery <trajano.juan@gmail.com>
2022-06-05 21:00:34 -0300

    refactor: :ambulance: Atualiza test_brazilian_jobs.py em /tests/brazilian
    Refatora a função test_brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -6,4 +6,5 @@ def test_brazilian_jobs():
     Test the Brazilian Jobs file.
     """
     jobs = read_brazilian_file('tests/mocks/brazilian_jobs.csv')
-    assert ('title' and 'salary' and 'type') in list(jobs[0].keys())
+    assert ('titulo' and 'salario' and 'tipo') not in jobs[0]
+    assert ('title' and 'salary' and 'type') in jobs[0]

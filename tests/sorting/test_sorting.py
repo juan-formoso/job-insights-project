0aae863889ecd39457d89209c5b21239d342178b
Avery <trajano.juan@gmail.com>
2022-06-05 21:24:05 -0300

    test: :sparkles: Atualiza test_sorting.py em /tests/sorting
    Implementa função test_sort_by_criteria
    
    Finaliza requisito 12

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,15 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+from src.jobs import read
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = read('src/jobs.csv')
+    
+    sort_by(jobs, 'min_salary')
+    assert jobs[0]['min_salary'] == '20000'
+
+    sort_by(jobs, 'max_salary')
+    assert jobs[0]['max_salary'] == '212901'
+
+    sort_by(jobs, 'date_posted')
+    assert jobs[0]['date_posted'] == '2020-05-08'

6590adff97d130b839201a3e57003b20bc6f9170
Avery <trajano.juan@gmail.com>
2022-06-05 21:05:09 -0300

    Finaliza requisito 11?

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -5,6 +5,10 @@ def test_brazilian_jobs():
     """
     Test the Brazilian Jobs file.
     """
-    jobs = read_brazilian_file('tests/mocks/brazilian_jobs.csv')
-    assert ('titulo' and 'salario' and 'tipo') not in jobs[0]
-    assert ('title' and 'salary' and 'type') in jobs[0]
+    jobs = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    assert 'titulo' not in jobs[0]
+    assert 'salario' not in jobs[0]
+    assert 'tipo' not in jobs[0]
+    assert 'title' in jobs[0]
+    assert 'salary' in jobs[0]
+    assert 'type' in jobs[0]

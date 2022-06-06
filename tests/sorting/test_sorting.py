cefd44b46af8188b8f7ead06098b6fba64af241c
Avery <trajano.juan@gmail.com>
2022-06-05 21:37:21 -0300

    Corrige erros de lint...

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -3,23 +3,23 @@ from src.jobs import read
 
 
 def test_sort_by_criteria():
-    # Referência do código abaixo: https://github.com/tryber/sd-014-b-project-job-insights/pull/14
+    #Ref: https://github.com/tryber/sd-014-b-project-job-insights/pull/14
     jobs = read("src/jobs.csv")[:10]
 
-    sort_by(all_jobs, "min_salary")
-    assert all_jobs[-1]["min_salary"] == ""
+    sort_by(jobs, "min_salary")
+    assert jobs[-1]["min_salary"] == ""
 
-    sort_by(all_jobs, "min_salary")
-    assert all_jobs[0]["min_salary"] == "20000"
+    sort_by(jobs, "min_salary")
+    assert jobs[0]["min_salary"] == "20000"
 
-    sort_by(all_jobs, "max_salary")
-    assert all_jobs[-1]["max_salary"] == ""
+    sort_by(jobs, "max_salary")
+    assert jobs[-1]["max_salary"] == ""
 
-    sort_by(all_jobs, "max_salary")
-    assert all_jobs[0]["max_salary"] == "212901"
+    sort_by(jobs, "max_salary")
+    assert jobs[0]["max_salary"] == "212901"
 
-    sort_by(all_jobs, "date_posted")
-    assert all_jobs[-1]["date_posted"] == "2020-04-25"
+    sort_by(jobs, "date_posted")
+    assert jobs[-1]["date_posted"] == "2020-04-25"
 
-    sort_by(all_jobs, "date_posted")
-    assert all_jobs[0]["date_posted"] == "2020-05-08"
+    sort_by(jobs, "date_posted")
+    assert jobs[0]["date_posted"] == "2020-05-08"
